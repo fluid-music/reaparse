8a00b060c8e09333516f6e51f451d72c62a0a986
Charles Holbrow <CharlesHolbrow@gmail.com>
2022-02-19 13:04:52 -0500

    switch testing assertion style to chai/expect

--- test/test-reaparse.js
+++ test/test-reaparse.js
@@ -1,12 +1,13 @@
 /* eslint-env mocha */
-import 'should';
 import 'mocha';
+import { should, expect } from 'chai';
+
+// should();
 
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 import rppp from 'rppp';
 import * as parsers from '../src/parsers.mjs';
-import should from 'should';
 
 import { FluidSession } from 'fluid-music';
 
@@ -16,7 +17,7 @@ const rppProject = rppp.parse(rppFileAsString)
 describe('createSimplifiedTracks', function () {
   it('should have two tracks', function () {
     const tracks = parsers.createSimplifiedTracks(rppProject);
-    tracks.length.should.equal(2);
+    expect(tracks).to.have.lengthOf(2);
   })
 })
 
@@ -25,22 +26,22 @@ describe('createFluidSession', function () {
   const fluidSession = parsers.createFluidSession(rppProject);
 
   it('should exist', function () {
-    should.exist(parsers.createFluidSession);
+    expect(parsers.createFluidSession).to.exist;
   });
 
   it('should create a FluidSession instance', function () {
-    should(fluidSession).be.an.instanceOf(FluidSession);
+    expect(fluidSession).to.be.an.instanceOf(FluidSession);
   });
 
   it('should extract the bpm of 90 from kit.RPP', function () {
-    fluidSession.bpm.should.equal(90);
+    expect(fluidSession.bpm).to.equal(90);
   });
 
   describe('parse tracks', function () {
     it('should extract two tracks named "kick", and "snare" respectively', function () {
-      fluidSession.tracks.length.should.equal(2);
-      fluidSession.tracks[0].name.should.equal('kick');
-      fluidSession.tracks[1].name.should.equal('snare');
+      expect(fluidSession.tracks.length).to.equal(2);
+      expect(fluidSession.tracks[0].name).to.equal('kick');
+      expect(fluidSession.tracks[1].name).to.equal('snare');
     })
 
     describe('kick track', function () {
@@ -49,12 +50,13 @@ describe('createFluidSession', function () {
 
       it('should have two instances of the kick drum', function () {
         const kickTrack = fluidSession.tracks[0];
-        kickTrack.name.should.equal('kick');
-        kickTrack.audioFiles.length.should.equal(2);
-        const [k1, k2] = kickTrack.audioFiles
-        k1.startInSourceSeconds.should.equal(0)
-        k1.startTimeSeconds.should.equal(0)
-        k2.startTimeSeconds.should.be.approximately(beatDurationInSeconds * 2.5, 0.00000001)
+        expect(kickTrack.name).to.equal('kick');
+        expect(kickTrack.audioFiles).to.have.lengthOf(2);
+
+        const [k1, k2] = kickTrack.audioFiles;
+        expect(k1.startInSourceSeconds).to.equal(0);
+        expect(k1.startTimeSeconds).to.equal(0);
+        expect(k2.startTimeSeconds).to.be.approximately(beatDurationInSeconds * 2.5, 0.00000001);
       })
     })
   })
