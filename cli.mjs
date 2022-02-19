f239eb8717d4d86df4c9827069f5ffb0f3d3e6c0
Charles Holbrow <CharlesHolbrow@gmail.com>
2022-02-18 20:50:50 -0500

    create structured data from RPP (cli only)

--- cli.mjs
+++ cli.mjs
@@ -7,6 +7,7 @@ import {
   getItemsInTrack,
   getSourcesInItem,
   getFirstParamByToken,
+  getTrackName,
 } from './src/parsers.mjs';
 
 const argv = yargs(process.argv.slice(2))
@@ -16,12 +17,14 @@ const argv = yargs(process.argv.slice(2))
 
 const argument = argv._[0];
 const reaperFilename = isAbsolute(argument)
-  ? argument 
+  ? argument
   : join(process.cwd(), argument);
 
-async function run() {
-  const rppProject = await parseRppFileFromFilename(reaperFilename);
+function getSimplifiedTracks(rppProject) {
+  const tracks = []
   for (const track of getTracksFromProject(rppProject)) {
+    const simpleTrackObject = {name: getTrackName(track), items: []}
+    tracks.push(simpleTrackObject)
     for (const item of getItemsInTrack(track)) {
       const itemName = getFirstParamByToken(item, 'NAME')
       for (const source of getSourcesInItem(item)) {
@@ -36,12 +39,25 @@ async function run() {
               durationSeconds,
               startInSourceSeconds,
             };
-            console.log(result);
+            simpleTrackObject.items.push(result)
           }
         }
       }
     }
   }
+
+  return tracks
+}
+
+async function run() {
+  const rppProject = await parseRppFileFromFilename(reaperFilename);
+  const tracks = getSimplifiedTracks(rppProject)
+  for (const track of tracks) {
+    for (const item of track.items) {
+      console.log(item.startInSourceSeconds)
+    }
+  }
+  console.dir(tracks, {depth: null})
 }
 
 run()
--- src/parsers.mjs
+++ src/parsers.mjs
@@ -1,29 +1,5 @@
 import rppp from 'rppp'
-import { readFile} from 'fs/promises'
-import fs from 'fs'
-
-async function old() {
-  const rppString = fs.readFileSync('./beat-it.RPP', 'utf-8')
-  const rppObj = rppp.parse(rppString)
-  const tracks = rppObj.contents.filter(o => o.token === 'TRACK')
-  const t1Items = tracks[1].contents.filter(o => o.token === 'ITEM')
-  const posObjects = t1Items
-    .map(o => o.contents.filter(o => o.token === 'POSITION'))
-    .map(array => array[0])
-  const positions = posObjects.map(o => o.params[0])
-
-  console.log(positions)
-  console.log('found:', positions.length)
-
-  // I rendered a wave file that "beat-it.wav" that trims the silence from the
-  // beginning. The following timing adjustment calculates the start time of
-  // all measures within "beat-it.wav"
-  const startInSession = positions[0]
-  const beatTimesInWav = positions.map(position => position - startInSession)
-
-  const commonjs = 'module.exports = ' + JSON.stringify(beatTimesInWav, null, 2)
-  fs.writeFileSync('./beat-it-measures.js', commonjs)
-}
+import { readFile } from 'fs/promises'
 
 export async function parseRppFileFromFilename(reaperFilename) {
   const reaperFileString = await readFile(reaperFilename, 'utf-8');
@@ -33,6 +9,10 @@ export async function parseRppFileFromFilename(reaperFilename) {
 
 export function getTracksFromProject(reaperProject) {
   return reaperProject.contents.filter(o => o.token === 'TRACK');
+}
+
+export function getTrackName(reaperTrack) {
+  return getFirstParamByToken(reaperTrack, 'NAME')
 }
 
 export function getItemsInTrack(reaperTrack) {
